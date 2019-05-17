217565bf295122488fa331aa7601e0debf19a754
Ahmet ATAY <ahmet@efabrika.com>
2019-05-17 12:00:27 +0300

    google analytics fix

--- docs/ga_worker.js
+++ docs/ga_worker.js
@@ -1,15 +1,14 @@
 const baseUrl = 'https://www.google-analytics.com/collect';
 const toQueryString = (params) => {
   return Object.keys(params).reduce((result, param) => {
-    result.push(`${param}=${params[param]}`);
+    result.push(`${param}=${encodeURIComponent(params[param])}`);
     return result;
   }, []).join('&');
 }
 
 onmessage = (e) => {
-  console.log('workerx', e.data.args);
   var request = new XMLHttpRequest();
-  request.open('POST', baseUrl+'?'+toQueryString(e.data.args), true);
+  request.open('POST', baseUrl+'?'+toQueryString({...e.data.args, z: 2}), true);
   request.send();
   request.onload = function () {
     postMessage(e.data);
